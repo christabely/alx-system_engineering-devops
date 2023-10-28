785844e3a35d6f6e6fe8e9461a480648d62ec33e
Christabel Aidoo <christabelaidoo71@gmail.com>
2023-10-27 22:18:30 -0700

    new commital

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,19 @@
-# install flask from pip3
-
-package { 'flask':
+# package resource for Flask using pip3
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
 
+# install pip3 if not already installed:
+
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+# Ensure pip3 is installed, and then install Flask with the specified version.
+class { 'python3-pip':
+  ensure => 'installed',
+} -> package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
